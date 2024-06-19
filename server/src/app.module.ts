552d4b0e130ddb1bccb221eb4fde59e5ad32d177
taptap-kor <tyy8282@gmail.com>
2024-06-19 19:56:38 +0900

    build: building whole structure

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TokenModule } from './token/token.module';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TokenModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
